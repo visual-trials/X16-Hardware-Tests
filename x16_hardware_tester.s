b4879188937581077431524f8b9ee29563eba4d8
Jeffrey <jeffrey@visual-trials.io>
2022-05-24 20:52:40 +0200

    moved x16 constants, cleanup

--- x16_hardware_tester.s
+++ x16_hardware_tester.s
@@ -1,40 +1,10 @@
 
+; ===========================
+; === X16 Hardware Tester ===
+; ===========================
+
 ; TODO: use ca65/cl65 instead of vasm6502_oldstyle
 
-; TODO: put these constants into a separate .inc/.s file
-
-VERA_ADDR_LOW     = $9F20
-VERA_ADDR_HIGH    = $9F21
-VERA_ADDR_BANK    = $9F22
-VERA_DATA0        = $9F23
-VERA_DATA1        = $9F24
-VERA_CTRL         = $9F25
-
-VERA_IEN          = $9F26
-VERA_ISR          = $9F27
-VERA_IRQLINE_L    = $9F28
-
-VERA_DC_VIDEO     = $9F29
-VERA_DC_HSCALE    = $9F2A
-VERA_DC_VSCALE    = $9F2B
-VERA_L0_CONFIG    = $9F2D
-VERA_L0_MAPBASE   = $9F2E
-VERA_L0_TILEBASE  = $9F2F
-
-VERA_L0_HSCROLL_L = $9F30
-VERA_L0_HSCROLL_H = $9F31
-VERA_L0_VSCROLL_L = $9F32
-VERA_L0_VSCROLL_H = $9F33
-
-VERA_PALETTE      = $1FA00
-
-VIA1_PORTB        = $9F00
-VIA1_PORTA        = $9F01
-VIA1_DDRB         = $9F02
-VIA1_DDRA         = $9F03
-
-; Note: when changing this, also change the bits in VERA_L0_CONFIG (see setup_vera_for_tile_map_rom_only.s)
-;         also check utils.s since we determine VRAM tile adresses based on the assumption the TILE_MAP_WIDTH = 128
 TILE_MAP_WIDTH = 128
 TILE_MAP_HEIGHT = 64
 
@@ -160,6 +130,7 @@ loop:
     
     ; === Included files ===
     
+    .include utils/x16.s
     .include utils/utils.s
     .include tests/fixed_ram_tests.s
     .include tests/banked_ram_tests.s
